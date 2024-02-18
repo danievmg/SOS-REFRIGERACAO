eb1fa1e09354eb15389ae2eab7e3509f381f2add
danievmg <danielcristianosilva553@gmail.com>
2024-02-18 20:25:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function alternarDiv() {
+    var div = document.getElementById('menuDiv');
+    if (div.style.display === 'none') {
+      div.style.display = 'block'; // Altera o estilo para 'block' para exibir a div
+    } else {
+      div.style.display = 'none'; // Altera o estilo para 'none' para esconder a div
+    }
+  }
+  
+// function Alteraicon(){
+//     var div = document.getElementById('icon');
+//     if(div.style.display === 'block'){
+//         div.innerHTML.i = 
+//     }
+// }
